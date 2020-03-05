4e0b22d30b03844b54fbff1a9674a96e9ffd53fc
juchani <victorfrancojuchani@gmail.com>
2020-03-05 17:15:24 -0400

    eliminacion de errores

--- 12.py
+++ 12.py
@@ -2,13 +2,10 @@ from gpiozero import LED, PingServer
 from gpiozero.tools import negated
 from signal import pause
 
-<<<<<<< HEAD
 green = LED(5)
 red = LED(5)
-=======
 green = LED(4)
 red = LED(6)
->>>>>>> c994cca9b1bf6ba0218d3a1b248c4630953c7c0d
 
 google = PingServer('google.com')
 
